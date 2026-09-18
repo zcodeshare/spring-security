bc06a57507d105d0a310a9ef6c396eb4c911f8b4
Jitendra Kumar <zitendrak1@gmail.com>
2026-09-18 18:33:27 +0530

    Updated

--- src/main/java/in/in2it/cats/springsecurity/config/SecurityConfig.java
+++ src/main/java/in/in2it/cats/springsecurity/config/SecurityConfig.java
@@ -2,42 +2,71 @@ package in.in2it.cats.springsecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration configuration) throws Exception {
+
+        return configuration.getAuthenticationManager();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.authorizeHttpRequests(auth -> auth
                 .requestMatchers(
-                        "/p1",
-                        "/p2",
-                        "/p3",
-                        "/p4",
-                        "/p5"
+                        "/public",
+                        "/test-authentication"
                 ).permitAll()
+                .requestMatchers("/user-only")
+                .hasRole("USER")
+                .requestMatchers("/admin-only")
+                .hasRole("ADMIN")
                 .anyRequest().authenticated()
-        ).httpBasic(Customizer.withDefaults());
+        ).formLogin(Customizer.withDefaults());
 
         return http.build();
     }
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
+    public InMemoryUserDetailsManager userDetailsManager(PasswordEncoder passwordEncoder) {
 
         UserDetails user = User
-                .withUsername("user")
-                .password("{noop}user123")
+                .withUsername("john")
+                .password(passwordEncoder.encode("john123"))
+                .roles("USER", "ADMIN")
+                .accountExpired(false)
+                .accountLocked(false)
+                .credentialsExpired(false)
+                .disabled(false)
+                .build();
+
+        UserDetails normalUser = User
+                .withUsername("alice")
+                .password(passwordEncoder.encode("alice123"))
                 .roles("USER")
                 .build();
 
-        return new InMemoryUserDetailsManager(user);
+        return new InMemoryUserDetailsManager(user, normalUser);
     }
 }
